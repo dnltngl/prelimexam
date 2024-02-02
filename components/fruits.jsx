8a21bb27a66949e10ccafe7624c51c5c51af5069
DnlTngl <99706377+dnltngl@users.noreply.github.com>
2024-02-02 14:26:52 +0800

    Add files via upload

--- components/fruits.jsx
+++ components/fruits.jsx
@@ -1,19 +1,11 @@
-import React from "react";
-
-const ThingstoBring = ({item, status}) =>{
-const backgroundColor = status ==='on-hand' ? 'pink':
-status ==='true' ? 'green': '';
-
-return (
-    <li style={{ fontFamily:'timesNewRoman', fontSize: '20px', backgroundColor, listStyle: 'none', padding: '8%', margin: '3%'}}>{{item}}</li>
-
-    );
-};
-
-const ListofItems = ({list, listStatus})=>{
-    return(
-        <ul>
-            {list.map((item, index) => )}</ul>
-<ThingstoBring key=
-        ))
+export default function fruits(props) {
+    return <>
+        <h1>Things to Bring</h1>
+        <h2>My Strawhat.</h2>
+        <h2 className={`${props.isgreen21? 'bg-green': 'bg-pink'} ${props.isHidden? 'hidden':''}`}>Correct!</h2>
+        <h2>Gum gum fruit</h2>
+        <h2 className={`${props.isgreen2? 'bg-green': 'bg-pink'} ${props.isHidden? 'hidden':''}`}>Correct!</h2>
+        <h2>The Red vest</h2>
+        <h2 className={`${props.ispink3? 'bg-green': 'bg-pink'} ${props.isHidden? 'hidden':''}`}>Incorrect!</h2>
+    </>
 }
